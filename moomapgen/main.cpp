bdf3cb12632180b01170ba95ba3d5af2c1690d15
tifeit <tifeit@gmail.com>
2015-05-12 15:19:01 +0300

    Initial commit from local git

---
+++ moomapgen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  moomapgen
+//
+//  Created by fomichem on 12.05.15.
+//  Copyright (c) 2015 tf8. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
